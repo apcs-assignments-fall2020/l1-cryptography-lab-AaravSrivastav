76266b5fa7cabaa755df04d41c996c07ea01b0fd
AaravSrivastav <22asrivastav@athenian.org>
2020-09-21 17:24:47 +0000

    added the ceaser encryption to main

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -2,23 +2,87 @@ import java.util.Scanner;
 
 public class Caesar {
     public static String encryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String encryptMsg = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            if((message.charAt(i) <= 'W' || (message.charAt(i) >= 'a' && message.charAt(i) <= 'w')) && message.charAt(i) != ' ')
+            {
+                encryptMsg += (char)(message.charAt(i)+3);
+            }
+            else if(message.charAt(i) == ' ')
+            {
+                encryptMsg += ' ';
+            }
+            else if((message.charAt(i)>='x' && message.charAt(i)>='z') || (message.charAt(i)>='X' && message.charAt(i)>='Z'))
+            {
+                encryptMsg += (char)(message.charAt(i)-23);
+            }
+            
+        }
+        return encryptMsg;
     }
 
     public static String decryptCaesar(String message) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String decryptMsg = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            if((message.charAt(i) >= 'd' || (message.charAt(i) >= 'D' && message.charAt(i) <= 'Z')) && message.charAt(i) != ' ')
+            {
+                decryptMsg += (char)(message.charAt(i)-3);
+            }
+            else if(message.charAt(i) == ' ')
+            {
+                decryptMsg += ' ';
+            }
+            else if((message.charAt(i)>='a' && message.charAt(i)>='c') || (message.charAt(i)>='A' && message.charAt(i)>='C'))
+            {
+                decryptMsg += (char)(message.charAt(i)+23);
+            }
+            
+        }
+        return decryptMsg;
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String encryptMsg = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            if((message.charAt(i) <= 90-key || (message.charAt(i) >= 'a' && message.charAt(i) <= 122-key)) && message.charAt(i) != ' ')
+            {
+                encryptMsg += (char)(message.charAt(i)+key);
+            }
+            else if(message.charAt(i) == ' ')
+            {
+                encryptMsg += ' ';
+            }
+            else if((message.charAt(i)>='x' && message.charAt(i)>='z') || (message.charAt(i)>='X' && message.charAt(i)>='Z'))
+            {
+                encryptMsg += (char)(message.charAt(i)-(26-key));
+            }
+            
+        }
+        return encryptMsg;
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        String decryptMsg = "";
+        for(int i=0; i<message.length(); i++)
+        {
+            if((message.charAt(i) >= 97+key || (message.charAt(i) >= 97+key && message.charAt(i) <= 'Z')) && message.charAt(i) != ' ')
+            {
+                decryptMsg += (char)(message.charAt(i)+key);
+            }
+            else if(message.charAt(i) == ' ')
+            {
+                decryptMsg += ' ';
+            }
+            else if((message.charAt(i)>='a' && message.charAt(i)>='c') || (message.charAt(i)>='A' && message.charAt(i)>='C'))
+            {
+                decryptMsg += (char)(message.charAt(i) - (26-key));
+            }
+            
+        }
+        return decryptMsg;
     }
 
 
